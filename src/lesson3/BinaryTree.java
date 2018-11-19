1644ffd66d4a0104feb70d1913fcfe598be34ed2
Виктор <sfera98@bk.ru>
2018-11-19 13:49:53 +0300

    3 lesson

--- src/lesson3/BinaryTree.java
+++ src/lesson3/BinaryTree.java
@@ -64,10 +64,87 @@ public class BinaryTree<T extends Comparable<T>> extends AbstractSet<T> implemen
      * Удаление элемента в дереве
      * Средняя
      */
+    // Трудоемкость: T = O(N) в худшем случае; T = O(logN) в среднем
+    // Ресурсоемкость: R = O(1)
     @Override
     public boolean remove(Object o) {
-        // TODO
-        throw new NotImplementedError();
+
+        if (root == null) {
+            return false;
+        }
+
+        Node<T> parent = root;
+        Node<T> moveable = root;
+
+        int num;
+        int moveableIsLeft = 0;
+
+        while ((num = moveable.value.compareTo((T) o)) != 0) {
+
+            parent = moveable;
+
+            if (num > 0) {
+                moveableIsLeft = 1;
+                moveable = moveable.left;
+            } else {
+                moveable = moveable.right;
+                moveableIsLeft = -1;
+            }
+            if (moveable == null) {
+                return false;
+            }
+        }
+
+        if (moveable.left != null && moveable.right != null) {
+
+            Node<T> next = moveable.right;
+            Node<T> before = moveable;
+
+            while (next.left != null) {
+                before = next;
+                next = next.left;
+            }
+            if (root == moveable) {
+                root = next;
+            }
+            if (before == moveable) {
+                moveable.right = next.right;
+            } else {
+                before.left = next.right;
+            }
+            next.left = moveable.left;
+            next.right = moveable.right;
+            switch (moveableIsLeft) {
+                case 1:
+                    parent.left = next;
+                    break;
+                case -1:
+                    parent.right = next;
+                    break;
+            }
+        } else {
+            if (moveable.right != null) {
+                link(parent, moveable, moveable.right);
+            } else if (moveable.left != null) {
+                link(parent, moveable, moveable.left);
+            } else {
+                link(parent, moveable, null);
+            }
+        }
+
+        size--;
+
+        return true;
+    }
+
+    private void link(Node<T> parent, Node<T> moveable, Node<T> child) {
+        if (parent == moveable) {
+            root = child;
+        } else if (parent.right == moveable) {
+            parent.right = child;
+        } else {
+            parent.left = child;
+        }
     }
 
     @Override
@@ -102,37 +179,53 @@ public class BinaryTree<T extends Comparable<T>> extends AbstractSet<T> implemen
 
         private Node<T> current = null;
 
-        private BinaryTreeIterator() {}
+        private BinaryTreeIterator() {
+            stack = new Stack<>();
+            addToStack(root);
+        }
 
         /**
          * Поиск следующего элемента
          * Средняя
          */
+        // Трудоемкость: T = O(1)
+        // Ресурсоемкость: R = O(N)
         private Node<T> findNext() {
-            // TODO
-            throw new NotImplementedError();
+            return stack.pop();
         }
+
+        private Stack<Node<T>> stack;
 
         @Override
         public boolean hasNext() {
-            return findNext() != null;
+            return !stack.isEmpty();
         }
 
         @Override
         public T next() {
             current = findNext();
-            if (current == null) throw new NoSuchElementException();
+            if (current.right != null) {
+                addToStack(current.right);
+            }
             return current.value;
+        }
+
+        private void addToStack(Node<T> root) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
         }
 
         /**
          * Удаление следующего элемента
          * Сложная
          */
+        // Трудоемкость: T = O(N) в худшем случае; T = O(logN) в среднем
+        // Ресурсоемкость: R = O(1)
         @Override
         public void remove() {
-            // TODO
-            throw new NotImplementedError();
+            BinaryTree.this.remove(current.value);
         }
     }
 
